319c17209f97119eb3f82ff36883a077f24f3f34
Chris Wood <wchristopherwood912@gmail.com>
2024-01-24 15:19:54 -0800

    frontend functional

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [peperoni, setPeperoni] = useState(false);
+  const [sausage, setSausage] = useState(false);
+  const [peppers, setPeppers] = useState(false);
+  const [onions, setOnions] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pizza Order</h1>
+      <form>
+        <input
+          type="text"
+          onChange={() => setName(name)}
+          placeholder="name"
+        ></input>
+        <label>
+          <input
+            type="checkbox"
+            checked={peperoni}
+            onChange={() => setPeperoni(!peperoni)}
+          ></input>
+          Pepperoni
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={sausage}
+            onChange={() => setSausage(!sausage)}
+          ></input>
+          Sausage
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={peppers}
+            onChange={() => setPeppers(!peppers)}
+          ></input>
+          Peppers
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={onions}
+            onChange={() => setOnions(!onions)}
+          ></input>
+          Onions
+        </label>
+      </form>
+      <div>
+        <h2>Your Order</h2>
+        {peperoni && <div>Pepperoni</div>}
+        {sausage && <div>Sausage</div>}
+        {peppers && <div>Peppers</div>}
+        {onions && <div>Onions</div>}
+      </div>
     </div>
   );
 }
